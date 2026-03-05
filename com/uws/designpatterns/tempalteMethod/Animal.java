0a6b60c47d447a4eddb5c248cc2eeb4f38fabf0a
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-03-05 10:20:12 +0000

    Exercise 2A: reviewed SortingAnimals strategy implementation and added explanatory comments

--- com/uws/designpatterns/tempalteMethod/Animal.java
+++ com/uws/designpatterns/tempalteMethod/Animal.java
@@ -1,9 +1,13 @@
 package com.uws.designpatterns.tempalteMethod;
 
+// Interface representing a generic animal
+// Any class implementing Animal must provide these behaviours
 public interface Animal {
 
+    // Returns true if the animal is a vertebrate (has a backbone)
 	public boolean isVertebrate();
 
+    // Returns the species name of the animal
 	public String getSpecies();
 
 }
--- com/uws/designpatterns/tempalteMethod/SortingAnimals.java
+++ com/uws/designpatterns/tempalteMethod/SortingAnimals.java
@@ -2,26 +2,46 @@ package com.uws.designpatterns.tempalteMethod;
 
 import java.util.Comparator;
 
+
 /**
  * Holds concrete strategies for working with animal lists.
+ * 
+ * This class attempts to implement the Strategy pattern,
+ * but the implementation is untidy because:
+ * - It uses anonymous inner classes
+ * - It uses raw Comparator types (no generics)
+ * - It requires casting from Object to Animal
+ * - Strategy logic is tightly coupled inside one class
  */
 public class SortingAnimals {
     
-       
+
+    // Private constructor prevents this class from being instantiated
+    // It is intended to act as a container for sorting strategies
     private SortingAnimals(){}
 
+
+    // Strategy for comparing animals based on whether they are vertebrates
+    // Uses an anonymous inner class implementing Comparator
     public final Comparator vertebrayStrategyFunc = new Comparator() {
         @Override
         public int compare(Object t, Object t1) {
+
+            // Cast objects to Animal because raw Comparator is used
             Animal firstAnimal = (Animal) t;
             Animal secondAnimal = (Animal) t1;
 
+            // If both animals have the same vertebrate status they are equal
             if (firstAnimal.isVertebrate()
                     == secondAnimal.isVertebrate()) {
                 return 0;
+
+            // If one of them is a vertebrate return 1
             } else if (firstAnimal.isVertebrate()
                     || secondAnimal.isVertebrate()) {
                 return 1;
+
+            // Otherwise return -1
             } else {
                 return -1;
             }
@@ -29,15 +49,18 @@ public class SortingAnimals {
     };
     
     
-
+    // Strategy for comparing animals alphabetically by species name
     public final Comparator speciesStrategyFunc = new Comparator() {
 
         @Override
         public int compare(Object t, Object t1) {
+
+            // Cast objects to Animal due to raw Comparator usage
             Animal firstAnimal = (Animal) t;
             Animal secondAnimal = (Animal) t1;
 
-           return firstAnimal.getSpecies().compareTo(secondAnimal.getSpecies());
+            // Compare species names using String comparison
+            return firstAnimal.getSpecies().compareTo(secondAnimal.getSpecies());
         }
     };
 }
